a45fbf6b7f5b081b22951ac137c780e083ca6d52
sdlyyxy <sdlyyxy@icloud.com>
2017-04-27 18:01:57 +0800

    Improve code and comment

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import urllib2,os,re,urllib,PyPDF2,pdb,time
+import urllib2,os,re,urllib,PyPDF2,time
 maxpdfs = 5	
 # the max number of issues of the paper on local disk 
 index='http://epaper.qlwb.com.cn/qlwb/pdf/'
 header='Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'
 workdir='/var/www/html/app/qlwb/'
+# the working directory, anywhere you like, will store the data and output the pdfs.
 
 def gettime():
 	s=time.strftime('%Y-%m-%d,%H:%M:%S',time.localtime(time.time()))
@@ -25,6 +26,7 @@ logfile.write('present pdfs '+str(checkpdfs())+'\n')
 logfile.close()
 #write a log of this python program began
 
+# download qlwb on _date
 def download(_date):
 	d_request = urllib2.Request(index+_date)
 	d_request.add_header('User-Agent', header)
@@ -34,34 +36,43 @@ def download(_date):
 	d_pages=len(d_match)
 
 	i=0
-	out=PyPDF2.PdfFileMerger()
+	out=PyPDF2.PdfFileMerger()# out is the pdf object we precess
 	out.strict=False
-	os.system('rm -rf '+workdir+'data/'+_date)
+	os.system('rm -rf '+workdir+'data/'+_date) #
 	os.mkdir(workdir+'data/'+_date)
+        # remove tmp folder stores the single page of pdfs and then create it
 	for s in d_match:
 		i+=1;
 		pdfpath=workdir+'data/'+_date+'/'+'%d'%i+'.pdf'
 		urllib.urlretrieve(index+s,pdfpath)
+                # download a page
+
 		tmppdf=PyPDF2.PdfFileReader(open(pdfpath,'rb'))
 		pdfinfo=tmppdf.getDocumentInfo()
 		if('C0' in pdfinfo.title):
 			break
+                # I only read A pages and B pages of qlwb... So the pages having titil 
+                # 'greater or equal' than C will be discarded
+
 		out.append(pdfpath,False)
 		if(i%10==0):
 			print 'Downloaded '+'%d'%i+' of all '+'%d'%d_pages+' pages......'
+                # simply shows the progress of downloading
+
 	print 'Download complete.Merging,this needs much time......'
 	out.write(workdir+_date+'.pdf')
+        # finally output the pdf
 	print 'Done.'
-# download qlwb on _date
 	
+# check qlwb of day _tmpdate whether exists
 def checkexist():
 	for root,dirs,files in os.walk(workdir):
 		if(root!=workdir):break
 		for file in files:
 			if(_tmpdate in file):return True
 	return False
-# check qlwb of day _tmpdate whether exists
 	
+# return a string which content is the filename of the oldest pdf on local disk
 def getoldest():
 	tmp='9999999999'
 	for root,dirs,files in os.walk(workdir):
@@ -69,51 +80,28 @@ def getoldest():
 		for file in files:
 			if(('pdf' in file)and(file<tmp)):tmp=file
 	return tmp
-# return a string which content is the filename of the oldest pdf on local disk
 
-# pdb.set_trace()
 
 request = urllib2.Request(index)
 request.add_header('User-Agent', header)
 html = urllib2.urlopen(request).read()
 pattern=re.compile(r'<A HREF="/qlwb/pdf/(........)...........</A><br>')
 alldates=pattern.findall(html)
-lastdate=len(alldates)-1
-_lastdate=alldates[lastdate]
+lastdate=len(alldates)-1 # An integer
 	
 	
+# download last five issues
 for tmpdate in range(lastdate-maxpdfs+1,lastdate+1):
 	_tmpdate=alldates[tmpdate]
 	if(not checkexist()):
 		download(_tmpdate)
-'''
-tmpdate=lastdate
-_tmpdate=alldates[tmpdate]
-while (checkpdfs()<maxpdfs):
-	download(_tmpdate)
-	tmpdate-=1
-	_tmpdate=alldates[tmpdate]
-
-#pdb.set_trace()
-	
-tmpdate=lastdate
-_tmpdate=alldates[tmpdate]
-while (not checkexist()):
-	#os.system('delete')
-	oldest=getoldest()
-	os.system('rm -rf '+workdir+'data/'+oldest[0:8])
-	os.system('rm -f '+oldest)
-	download(_tmpdate)
-	tmpdate-=1
-	_tmpdate=alldates[tmpdate]
-'''
 
 	
 logfile=open(workdir+'log','a')
 logfile.write(gettime()+'download completed\n')
 logfile.write('present pdfs '+str(checkpdfs())+'\n')
 while(checkpdfs()>maxpdfs):
-#delete old files
+#delete the oldest issue and its related data
 	logfile.write(gettime()+' present pdfs: '+str(checkpdfs())+'\n')
 	tmp='99999999'
 	for root,dirs,files in os.walk(workdir):
@@ -125,9 +113,3 @@ while(checkpdfs()>maxpdfs):
 logfile.write(gettime()+'clear old completed\n')
 logfile.write('present pdfs '+str(checkpdfs())+'\n')
 logfile.close()
-#tmp='99999999'
-#datadir=workdir+'data/'
-#for root,dirs,files in os.walk(datadir):
-#	if(root!=datadir):break
-#	for dir in dirs:
-#		if(('	
